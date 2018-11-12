f742da862f60860f859a674bcfc495210a73b63a
Niklas <me@sodapop.se>
2018-11-12 19:15:02 +0100

    --wip-- [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate scraper;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate regex;
 extern crate serde_json;
 
 // use reqwest;
@@ -28,50 +29,74 @@ struct SVTVideo {
     videoReferences: Vec<VideoReference>,
 }
 
+enum Service {
+    tv4,
+    svt,
+}
+
+fn determine_service(url: &str) -> Option<Service> {
+    let re_tv4 = regex::Regex::new(r"^https?://(?:www\.)?tv4(?:play)?\.se/.*(?:-|/)(\d+)");
+    let re_svt = regex::Regex::new(r"^https?://(?:www\.)?(?:svt|svtplay|oppetarkiv)\.se/");
+
+    if re_tv4.unwrap().is_match(url) {
+        return Some(Service::tv4);
+    }
+    if re_svt.unwrap().is_match(url) {
+        return Some(Service::svt);
+    }
+
+    return None;
+}
+
 fn visit_website(url: &str) {
     let request = reqwest::get(url);
     let text = request.unwrap().text().unwrap();
 
-    let document = Html::parse_document(&text);
-    let id_selector =
-        Selector::parse("video[data-video-id], a[data-json-href], iframe[src]").unwrap();
+    match determine_service(url) {
+        Some(Service::svt) => {
+            let document = Html::parse_document(&text);
+            let id_selector =
+                Selector::parse("video[data-video-id], a[data-json-href], iframe[src]").unwrap();
 
-    // if (element.value().nam)
+            // if (element.value().nam)
 
-    for element in document.select(&id_selector) {
-        let element_name = element.value().name();
+            for element in document.select(&id_selector) {
+                let element_name = element.value().name();
 
-        let video_id = match element_name {
-            "video" => element.value().attr("data-video-id"),
-            "a" => {
-                // TODO
-                let attr = element.value().attr("data-json-href");
-                attr
+                let video_id = match element_name {
+                    "video" => element.value().attr("data-video-id"),
+                    "a" => {
+                        // TODO
+                        let attr = element.value().attr("data-json-href");
+                        attr
+                    }
+                    "iframe" => {
+                        // TODO
+                        let attr = element.value().attr("src");
+                        attr
+                    }
+                    _ => None,
+                };
+
+                println!("{:?}", video_id);
+
+                let mut data_url = [
+                    "https://api.svt.se/videoplayer-api/video/",
+                    video_id.unwrap(),
+                ]
+                    .concat();
+                println!("{:?}", data_url);
+
+                let request = reqwest::get(&data_url);
+                let text = request.unwrap().text().unwrap();
+
+                let deserialized: SVTVideo = serde_json::from_str(&text).unwrap();
+                println!("{:?}", deserialized);
+
+                // FIXME Get url here to construct ffmpeg
             }
-            "iframe" => {
-                // TODO
-                let attr = element.value().attr("src");
-                attr
-            }
-            _ => None,
-        };
-
-        println!("{:?}", video_id);
-
-        let mut data_url = [
-            "https://api.svt.se/videoplayer-api/video/",
-            video_id.unwrap(),
-        ]
-            .concat();
-        println!("{:?}", data_url);
-
-        let request = reqwest::get(&data_url);
-        let text = request.unwrap().text().unwrap();
-
-        let deserialized: SVTVideo = serde_json::from_str(&text).unwrap();
-        println!("{:?}", deserialized);
-
-        // FIXME Get url here to construct ffmpeg
+        }
+        _ => println!("Invalid service"),
     }
 
     // let mut dataUrl = "https://api.svt.se/videoplayer-api/video/" + videoId;
@@ -93,7 +118,7 @@ impl EventHandler {
             } else {
                 Command::new("sh")
                     .arg("-c")
-                    .arg("echo lol")
+                    .arg("gtimeout 1s top")
                     .stdout(Stdio::piped())
                     .spawn()
                     .unwrap()
@@ -123,7 +148,9 @@ impl sciter::EventHandler for EventHandler {
 
 fn main() {
     let handler = EventHandler {};
-    let mut frame = sciter::Window::new();
+    let mut frame = sciter::WindowBuilder::main_window()
+        .with_size((512, 512))
+        .create();
     frame.event_handler(handler);
     frame.load_file("html/main.html");
     frame.run_app();
